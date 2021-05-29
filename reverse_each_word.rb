5484aeba2d3ffccff53d4eccbd12a1c0d054d1db
Noah  Klem <klem.noah@gmail.com>
2021-05-29 01:41:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   new_reverse_string_array = string.reverse.split
   new_reverse_string_array.collect do |word|
-    new_reverse_string_array.reverse.join(" ")
+    return new_reverse_string_array.reverse.join(" ")
   end
 end
